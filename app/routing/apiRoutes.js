7e1fbbfd7879c433a78a789cf2e3e28dad5c02c1
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-02-26 17:36:42 -0500

    added to routes files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,33 @@
-// 1. A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
-// 2. A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+// path is a built in npm package
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+
+
+app.get("/api/friends", function(req, res) {
+  res.json(res);
+});
+
+
+app.post("/api/friends", function(req, res) {
+  var newPerson = req.body;
+
+  console.log(newPerson);
+
+  res.json(newPerson);
+});
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,27 @@
-// 1. A GET Route to /survey which should display the survey page.
-// 2. A default USE route that leads to home.html which displays the home page.
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+// path is a built in npm package
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
