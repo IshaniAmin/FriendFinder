8b41534c89579ef84c0cd7551c28094bc231ed28
Ishani Amin <ishaniamin@Ishanis-MacBook-Pro.local>
2017-02-26 21:54:00 -0500

    added friend matcher

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,8 +10,39 @@ module.exports = function(app) {
 
 
 	app.post("/api/friends", function(req, res) {
-	  friendsList.push(req.body)
-	  res.json(friendsList);
+
+		var friendMatch = {
+	  		name: "",
+	  		photo: "",
+	  		difference: 200
+	  	}
+
+		var newPerson = req.body;
+
+		var newName = newPerson.name;
+		var newPhoto = newPerson.photo;
+		var newScore = newPerson.scores;
+
+		var totalDifference;
+
+		for (var i=0; i<friendsList.length-1; i++) {
+	  		console.log(friendsList[i].name);
+
+	  		for(var j = 0; j < 10; j++){
+	  			totalDifference += Math.abs(parseInt(newScore[j]) - parseInt(friendsList[i].scores[j]));
+
+	  			if (totalDifference <= friendMatch.difference) {
+		  			friendMatch.name = friendsList[i].name;
+		  			friendMatch.photo = friendsList[i].photo;
+		  			friendMatch.difference = totalDifference;
+	  			}
+	  		}
+
+	  	}
+
+	friendsList.push(newPerson);
+	res.json(friendMatch);
+
 	});
 
 }
